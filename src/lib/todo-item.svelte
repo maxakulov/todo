ab612882a5578bd4842ce07064865d2fbd872e15
Max Akulov <maxavsy@gmail.com>
2022-04-02 14:45:58 +0000

    Develop HTML structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="">
+        <button aria-label="Done / Not Done">Done / Not Done</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text">
+        <button aria-label="Save">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete">Delete</button>
+    </form>
+</div>
